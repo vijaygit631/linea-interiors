7ce0d3daa7f882971e07e36abbab1e37f361ffc0
vijaygit631 <vijaybabutirumalasetti@gmail.com>
2026-06-21 22:17:00 +0530

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,40 +9,11 @@ function Footer() {
       <div className="max-w-7xl mx-auto px-8">
 
         <div className="flex flex-col items-center">
-
           <img
             src={logo}
             alt="Linéa"
             className="w-32 mb-12"
           />
-
-          <div className="flex gap-10 text-4xl mb-12">
-
-            <a
-              href="designs_by_linea"
-              target="_blank"
-              className="hover:text-[#B89B67] duration-300"
-            >
-              <FaInstagram />
-            </a>
-
-            <a
-              href="https://wa.me/918688331102"
-              target="_blank"
-              className="hover:text-[#B89B67] duration-300"
-            >
-              <FaWhatsapp />
-            </a>
-
-            <a
-              href="mailto:lineainteriorshyd@gmail.com"
-              className="hover:text-[#B89B67] duration-300"
-            >
-              <MdEmail />
-            </a>
-
-          </div>
-
           <div className="text-center text-gray-400 space-y-2">
 
             <p>
